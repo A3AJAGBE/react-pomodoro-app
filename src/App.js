2364bbb57ca2b726270e0cb8318a26eef5861198
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-12-13 20:16:53 +0000

    Add new imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from 'react-bootstrap/Button';
 
 const App = () => {
 	return (
 		<Container fluid id="App-Container">
-			<h1>hey</h1>
+			
+			<div className="App-Div text-center">
+				<h1>Pomodoro App</h1>
+				<span className="lead"> 25 + 5 Time Management Clock </span>
+
+				<Card className="" id="App-Card">
+					<Card.Header>Session</Card.Header>
+					<Card.Body>
+						
+						<Card.Text>
+							00 : 00
+						</Card.Text>
+						
+					</Card.Body>
+					<Card.Footer className="text-muted">
+						<ButtonGroup size="lg" className="d-grid gap-2 d-md-block">
+							<Button variant="dark">Left</Button>
+							<Button variant="danger">Middle</Button>
+							<Button variant="warning">Right</Button>
+						</ButtonGroup>
+					</Card.Footer>
+				</Card>
+			</div>
 		</Container>
 	);
 }
