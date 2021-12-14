7c7a39e6fa839d429c4de1323b594ee7e1fd20b8
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-12-14 19:59:14 +0000

    Add settings div

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const App = () => {
 	return (
@@ -29,6 +31,26 @@ const App = () => {
 						<Button className="col-4 m-0 rounded-0 Card-Btn" id="stop">Stop</Button>
 					</Card.Footer>
 				</Card>
+
+				<div class="mx-auto m-3" id="Settings">
+					<Row>
+						<Col md={6}>
+							<div className="mb-3">
+								<p>Work Time</p>
+								<Button className="me-2">⬆</Button> <span>25</span> <Button className="ms-2">⬇️</Button>
+							</div>
+							
+						</Col>
+
+						<Col md={6}>
+							<div className="mb-3">
+								<p>Break Time</p>
+								<Button className="me-2">⬆</Button> <span>5</span> <Button className="ms-2">⬇️</Button>
+							</div>
+						</Col>
+					</Row>
+				</div>
+
 			</div>
 		</Container>
 	);
