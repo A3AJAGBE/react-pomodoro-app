10eb5e83f0debba2193e3a8c805d3149d7d2e769
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-12-15 20:13:44 +0000

    Add fontawesome icons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUp, faArrowDown, faPlay, faPause, faRedoAlt} from '@fortawesome/free-solid-svg-icons'
+
+const ArrowUp = <FontAwesomeIcon icon={faArrowUp} />
+const ArrowDown = <FontAwesomeIcon icon={faArrowDown} />
+const Start = <FontAwesomeIcon icon={faPlay} />
+const Pause = <FontAwesomeIcon icon={faPause} />
+const Reset = <FontAwesomeIcon icon={faRedoAlt} />
 
 const App = () => {
 	return (
@@ -24,11 +32,12 @@ const App = () => {
 						
 					</Card.Body>
 					<Card.Footer className="flex-nowrap p-0">
-						<Button className="col-4 m-0 rounded-0 Card-Btn" id="start_stop">Start</Button>
+						<Button className="col-4 m-0 rounded-0 Card-Btn" id="start_stop">{Start} <span className='ps-1'>Start</span></Button>
 
-						<Button className="col-4 m-0 rounded-0 Card-Btn" id="reset">Reset</Button>
+						<Button className="col-4 m-0 rounded-0 Card-Btn" id="pause">{Pause} <span className='ps-1'>Pause</span> </Button>
 
-						<Button className="col-4 m-0 rounded-0 Card-Btn" id="stop">Stop</Button>
+						<Button className="col-4 m-0 rounded-0 Card-Btn" id="reset">{Reset} <span className='ps-1'>Reset</span></Button>
+
 					</Card.Footer>
 				</Card>
 
@@ -37,7 +46,7 @@ const App = () => {
 						<Col md={6}>
 							<div className="mb-3" >
 								<p id="session-label">Work Time</p>
-								<Button className="me-2" id="session-increment">⬆</Button> <span id="session-length">25</span> <Button className="ms-2" id="session-decrement">⬇️</Button>
+								<Button className="me-2" id="session-increment"> {ArrowUp} </Button> <span id="session-length">25</span> <Button className="ms-2" id="session-decrement">{ArrowDown}</Button>
 							</div>
 							
 						</Col>
@@ -45,7 +54,7 @@ const App = () => {
 						<Col md={6}>
 							<div className="mb-3" >
 								<p id="break-label">Break Time</p>
-								<Button className="me-2" id="break-increment">⬆</Button> <span  id="break-length">5</span> <Button className="ms-2" id="break-decrement">⬇️</Button>
+								<Button className="me-2" id="break-increment"> {ArrowUp} </Button> <span  id="break-length">5</span> <Button className="ms-2" id="break-decrement">{ArrowDown}</Button>
 							</div>
 						</Col>
 					</Row>
