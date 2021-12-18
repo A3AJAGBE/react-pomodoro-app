973585e233588aeb05279ab5460644a552f51505
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-12-18 01:46:11 +0000

    Add Pomodoro func

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import {Container, Card, Button, Row, Col} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp, faArrowDown, faPlay, faPause, faRedoAlt} from '@fortawesome/free-solid-svg-icons';
 
+const Pomodoro = () => {
+	return (
+		<div className="App-Div text-center">
+			<h1 className='display-1'>Pomodoro App</h1>
+			<h2 className="display-6"> 25 + 5 Time Management Clock </h2>
+		</div>
+	);
+}
 
 const App = () => {
 	return (
 		<Container fluid id="App-Container">
-			
+			<Pomodoro />
 		</Container>
 	);
 }
