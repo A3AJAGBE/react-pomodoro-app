e8ddf1661faa4b474a64382d11dd25867ee2b1f5
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-12-18 01:52:41 +0000

    Add card content

--- src/App.js
+++ src/App.js
@@ -5,11 +5,45 @@ import {Container, Card, Button, Row, Col} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp, faArrowDown, faPlay, faPause, faRedoAlt} from '@fortawesome/free-solid-svg-icons';
 
+
+const ArrowUpIcon = <FontAwesomeIcon icon={faArrowUp} />
+const ArrowDownIcon = <FontAwesomeIcon icon={faArrowDown} />
+const StartIcon = <FontAwesomeIcon icon={faPlay} />
+const PauseIcon = <FontAwesomeIcon icon={faPause} />
+const ResetIcon = <FontAwesomeIcon icon={faRedoAlt} />
+
+
+const Dashboard = () => {
+	return (
+		<Card className="" id="App-Card">
+			<Card.Header id="timer-label">sesLabel</Card.Header>
+			<Card.Body>
+				
+				<Card.Text id="time-left">
+					state
+				</Card.Text>
+				
+			</Card.Body>
+			<Card.Footer className="flex-nowrap p-0">
+				<Button className="col-4 m-0 rounded-0 Card-Btn" id="start_stop"> {StartIcon} <span className='ps-1'>Start</span></Button>
+
+				<Button className="col-4 m-0 rounded-0 Card-Btn" id="pause"> {PauseIcon} <span className='ps-1'>Pause</span> </Button>
+
+				<Button className="col-4 m-0 rounded-0 Card-Btn" id="reset"> {ResetIcon} <span className='ps-1'>Reset</span></Button>
+
+			</Card.Footer>
+		</Card>
+	);
+}
+
+
 const Pomodoro = () => {
 	return (
 		<div className="App-Div text-center">
 			<h1 className='display-1'>Pomodoro App</h1>
 			<h2 className="display-6"> 25 + 5 Time Management Clock </h2>
+
+			<Dashboard />
 		</div>
 	);
 }
